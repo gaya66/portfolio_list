bd164297e052f87f3b53b55914ce128893203035
gaya <krgysn@gmail.com>
2021-01-25 21:09:46 +0900

    DockerのTime zoneをJSTに設定.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module Myapp
     config.i18n.default_locale = :ja
     # タイムゾーンも変更するなら，以下を追加
     config.time_zone = 'Tokyo'
-
+    config.active_record.default_timezone = :local
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
