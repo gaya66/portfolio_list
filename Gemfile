f0ca9531a916a3aeee129cc8a0ae6bea0b9b21b9
gaya <krgysn@gmail.com>
2021-01-25 18:35:18 +0900

    docker exitedエラー解決

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'devise'
+gem 'devise-i18n' # 日本語化
+gem 'rails-i18n', '~> 5.1' # 日本語化
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # 以下を追加すれば日本語に
+    config.i18n.default_locale = :ja
+    # タイムゾーンも変更するなら，以下を追加
+    config.time_zone = 'Asia/Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
