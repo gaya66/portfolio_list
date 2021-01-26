28a3c5001adf509472dfd3decc020f919c4ccca4
gaya <krgysn@gmail.com>
2021-01-26 09:16:19 +0900

    現在のパスワードを入力しなくてもユーザー情報を変更できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'users/index'
   root 'portfolios#index'
-  devise_for :users
+  devise_for :users , controllers: { registrations: 'registrations' }  # 現在のパスワードを入力しなくても更新できるようにするため
   resources :users, :only => [:index, :show]
   resources :portfolios
   # root 'portfolios#show'
